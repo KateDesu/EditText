b54f8d2c3c70201be563e866098816723157a435
Екатерина Петрова <katedesu1@yandex.ru>
2025-03-31 22:04:58 +0500

    Добавление TextView

--- app/src/main/java/com/practicum/edittext/MainActivity.kt
+++ app/src/main/java/com/practicum/edittext/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.practicum.edittext
 
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.LinearLayout
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,10 +15,35 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.container)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val linearLayout = findViewById<LinearLayout>(R.id.container)
+        val inputEditText = findViewById<EditText>(R.id.inputEditText)
+        val clearButton = findViewById<ImageView>(R.id.clearIcon)
+
+        clearButton.setOnClickListener {
+            inputEditText.setText("")
+        }
+    }
+
+    private fun clearButtonVisibility(s: CharSequence?): Int {
+        return if (s.isNullOrEmpty()) {
+            View.GONE
+        } else {
+            View.VISIBLE
+        }
+    }
+
+    private fun isPrime(number: Int): Boolean {
+        for (i in 2..number / 2) {
+            if (number % i == 0) {
+                return false
+            }
+        }
+        return true
     }
 }
